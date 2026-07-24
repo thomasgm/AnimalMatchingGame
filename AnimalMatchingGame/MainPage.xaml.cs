2a5d630067261f6aa3cca04b3e1e8fdd1907f457
Thomas Gonzalez Miranda <thomasgm@gmail.com>
2026-07-24 09:39:44 -0300

    Add project files.

---
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            AnimalButtons.IsVisible = false;
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐒","🐒",
+                "🐶","🐶",
+                "🐱","🐱",
+                "🦊","🦊",
+                "🐻","🐻",
+                "🐼","🐼",
+                "🐨","🐨",
+                "🐯","🐯"
+            };
+            foreach (Button button in AnimalButtons.Children.OfType<Button>())
+            {
+                if (animalEmoji.Count == 0)
+                {
+                    break;
+                }
+                int index = new Random().Next(animalEmoji.Count);
+                button.Text = animalEmoji[index];
+                animalEmoji.RemoveAt(index);
+            }
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
